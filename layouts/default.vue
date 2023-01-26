00dced54983cecd2523345bb81211c867d5d41fa
Mab Hel <h-mab@beniten.com>
2023-01-26 23:46:52 +0700

    Added a simple page

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <nav>
+        <ul class="d-flex">
+            <li><a href="/">Home</a></li>
+            <li><a href="/example">Example</a></li>
+        </ul>
+    </nav>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Default",
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/example.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Example Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Example",
+};
+</script>
+
+<style lang="scss" scoped></style>
