e3cc8bf565cfe6f1e520ff2d2e589f616c77fdb6
Mab Hel <h-mab@beniten.com>
2023-01-31 23:39:47 +0700

    Revise layouts

--- components/Layouts/Header.vue
+++ components/Layouts/Header.vue
@@ -2,9 +2,9 @@
   <LayoutsAnnoucement />
   <header class="h-[280px] bg-white">
     <div class="container mx-auto flex items-center h-full">
-      <div class="space-y-2">
-        <h1 class="text-3xl">Basic WordPress Course</h1>
-        <p class="text-lg">Learn how to create website with us!</p>
+      <div class="space-y-5">
+        <h1 class="text-3xl font-bold">វីដេអូបង្រៀនមូលដ្ឋានគ្រឹះ WordPress</h1>
+        <p class="text-lg">រៀនពីរបៀបក្នុងបង្កើតគេហទំព័រជាមួយពួកយើង!</p>
         <div class="mt-5">
           <a
             href="https://forms.gle/5d5LfzNjM61Vb8Xs5"
@@ -12,7 +12,7 @@
             target="_blank"
             class="bg-gray-800 text-white rounded-lg px-4 py-2 focus:bg-gray-900 focus:ring-1 focus:ring-offset-2"
           >
-            Enroll Now
+            ចុះឈ្មោះឥឡូវ
           </a>
         </div>
       </div>
