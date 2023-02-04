fb6d4bed3b4a6329c08dea6d046f32b242be24af
Mab Hel <h-mab@beniten.com>
2023-02-04 17:56:51 +0700

    Added SEO meta tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,27 +2,85 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Home - Programming With Mab',
+      title: "Home - Programming With Mab",
       meta: [
-        { name: 'title', content: 'Home - Programming With Mab' },
-        { name: 'description', content: 'Learn how to a create website with us!' },
-        { name: 'keywords', content: 'wordpress, php, web development, theme development' },
-        { name: 'robots', content: 'index, follow' },
-        { name: 'language', content: 'English' },
+        { name: "title", content: "Home - Programming With Mab" },
+        {
+          name: "description",
+          content: "Learn how to a create website with us!",
+        },
+        {
+          name: "keywords",
+          content: "wordpress, php, web development, theme development",
+        },
+        { name: "robots", content: "index, follow" },
+        { name: "language", content: "English" },
 
-        { name: 'og:type', content: 'website' },
-        { name: 'og:url', content: 'https://programmingwithmab.helmab.info/' },
-        { name: 'og:title', content: 'Programming With Mab' },
-        { name: 'og:description', content: 'Learn how to create a website with us!' },
-        { name: 'og:image', content: 'https://programmingwithmab.helmab.info/img/logo.jpg' },
+        { name: "og:type", content: "website" },
+        { name: "og:url", content: "https://programmingwithmab.helmab.info/" },
+        { name: "og:title", content: "Programming With Mab" },
+        {
+          name: "og:description",
+          content: "Learn how to create a website with us!",
+        },
+        {
+          name: "og:image",
+          content: "/img/logo.jpg",
+        },
 
-        { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:url', content: 'https://programmingwithmab.helmab.info/' },
-        { name: 'twitter:title', content: 'Programming With Mab' },
-        { name: 'twitter:description', content: 'Learn how to create a website with us!' },
-        { name: 'twitter:image', content: 'https://programmingwithmab.helmab.info/img/logo.jpg' },
+        { name: "twitter:card", content: "summary_large_image" },
+        {
+          name: "twitter:url",
+          content: "https://programmingwithmab.helmab.info/",
+        },
+        { name: "twitter:title", content: "Programming With Mab" },
+        {
+          name: "twitter:description",
+          content: "Learn how to create a website with us!",
+        },
+        {
+          name: "twitter:image",
+          content: "/img/logo.jpg",
+        },
+
+        {
+          name: "msapplication-TileColor",
+          content: "#da532c",
+        },
+        {
+          name: "theme-color",
+          content: "#ffffff",
+        },
       ],
-    }
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest",
+        },
+        {
+          rel: "mask-icon",
+          href: "/safari-pinned-tab.svg",
+          color: "#5bbad5"
+        }
+      ]
+    },
   },
   css: ["~/assets/css/main.css"],
   postcss: {
